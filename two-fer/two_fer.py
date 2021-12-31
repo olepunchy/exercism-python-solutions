34b506be86cb0cbbd3e0e5e2aa1893d73f7c5701
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-12-31 12:15:57 -0600

    Updated .gitignore

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,10 @@
-def two_fer(name):
-    pass
+"""Two Fer from Exercism"""
+
+
+def two_fer(name="you"):
+    """Print a message based on the input of name
+
+    parameter: str name
+    return: str formatted string
+    """
+    return f"One for {name}, one for me."
